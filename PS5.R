691d825f8a4b9312a8db85a3a6960b852dbeb615
beisenpress <ben@eisenpress.com>
2015-10-28 21:59:37 -0500

    Text of Question 1

---
+++ PS5.R
@@ -0,0 +1,16 @@
+# 41100 PS5
+
+##################### Question 1 ###########################
+# 1 Can observational studies replicate experiments?
+# In class we studied data from the National Supported Work (NSW) experiment. Men were 
+# randomized to either receive job training (the treatment) or not (control). We found the 
+# average treatment effect was $1,794.34: treated men could expect to earn this much more than 
+# controls. What if we didn’t have a randomized experiment? Could we still estimate this causal
+# effect from observational data? Answering these questions is the goal of this problem.
+
+# Suppose that the same 185 men received job training, and we want to know what 
+# effect this training had on income, but we do not have access to the NSW control sample. 
+# That is, no experiment was done. We need a control sample to compare too, and for this we 
+# have 2490 men drawn from the Panel Study of Income Dynamics (PSID). This is now observational 
+# data. The full data consists of the 185 treated men and these 2490 men to act as controls 
+# (file nsw_psid.csv).
